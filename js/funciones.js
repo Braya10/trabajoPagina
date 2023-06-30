7136e8552c548da26714c067deb9a7f4f91a1abf
braya10 <brayanamc10@gmail.com>
2023-06-29 21:57:46 -0500

    Version 3

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,4 @@
-var Ingreso;
+var antiguedad;
 var valor;
 
 
@@ -19,8 +19,23 @@ function calculoPermanencia() {
     var fechaActual = new Date();
     var fechaIngresoObjeto = new Date(fechaIngreso);
     var diferenciaTiempo = fechaActual - fechaIngresoObjeto;
-    Ingreso = Math.floor(diferenciaTiempo / (1000 * 60 * 60 * 24 * 365.25));
+    antiguedad = Math.floor(diferenciaTiempo / (1000 * 60 * 60 * 24 * 365.25));
 
-    document.getElementById("resultadoPermanencia").innerHTML = "La antiguedad es: " + Ingreso + " años";
+    document.getElementById("resultadoPermanencia").innerHTML = "La antiguedad es: " + antiguedad + " años";
 
 }
+
+function modificarValor() {
+    valor = prompt("Ingrese un valor:");
+
+    document.getElementById("resultadoValor").innerHTML = valor;
+}
+
+function calcularPrestaciones() {
+    // var antiguedad = document.getElementById("resultadoAntiguedad").value;
+    // var salario = document.getElementById("resultadoValor").value;
+
+    var prestaciones = (antiguedad * valor) / 12;
+
+    document.getElementById("resultadoPrestaciones").innerHTML = "Prestaciones: $" + prestaciones.toFixed(2) ;
+  }
